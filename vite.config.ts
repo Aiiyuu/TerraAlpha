edfa2c2f0a9f81c795e78a57ced7d0e2d6edc3de
Nazariy <nazariy.holovach2007@gmail.com>
2025-11-12 00:20:19 +0200

    rename project

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: '/Testing_game/', // This should match GitHub repo name
+  base: "/TerraAlpha/", // This should match GitHub repo name
 });
